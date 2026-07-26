50d05fa7f7ee29c92716993107457cf27cce86de
Dom <camposdominicelec@gmail.com>
2026-07-26 14:36:32 -0700

    Add comments to gibblesort function
    Updated gibblesort function to include comments for clarity.

--- gibble_sort.py
+++ gibble_sort.py
@@ -1,11 +1,11 @@
 import random
 
-def gibblesort(arr):
+def gibblesort(arr): #Please ignore how arr is merely decorative.
 	values = ["1", "2", "3"] #Defines what the sort should look for and match.
 
 	original_values = list(values)
 
-	print(f"Starting Values: {original_values}")
+	print(f"Starting Values: {original_values}") #Prints what was given to GibbleSort
 
 	while True:
 		random.shuffle(values) #Shuffles the values and shows the output.
